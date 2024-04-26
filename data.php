06988ec1c588c9a8d5ba5ec424a25debc2b67e73
rifqighazyfr <rifqighazyfr@gmail.com>
2024-04-26 16:17:47 +0800

    first commit

---
+++ data.php
@@ -0,0 +1,63 @@
+<div
+    class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Table</h1>
+</div>
+<div>
+    <div class="card">
+        <div class="card-body">
+            <div class="container">
+                <div class="row bg-dark text-white">
+                    <div class="col border">
+                        <h4>No.</h4>
+                    </div>
+                    <div class="col border">
+                        <h4>Badge ID</h4>
+                    </div>
+                    <div class="col border">
+                        <h4>Nama</h4>
+                    </div>
+                    <div class="col border">
+                        <h4>Username</h4>
+                    </div>
+                    <div class="col border">
+                        <h4>Nomor HP</h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col border">
+                        <h4>1</h4>
+                    </div>
+                    <div class="col border">
+                        <h4>12345</h4>
+                    </div>
+                    <div class="col border">
+                        <h4>Adam Malik</h4>
+                    </div>
+                    <div class="col border">
+                        <h4></h4>
+                    </div>
+                    <div class="col border">
+                        <h4>081234567890</h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col border">
+                        <h4>2</h4>
+                    </div>
+                    <div class="col border">
+                        <h4>12346</h4>
+                    </div>
+                    <div class="col border">
+                        <h4>Awang Long</h4>
+                    </div>
+                    <div class="col border">
+                        <h4></h4>
+                    </div>
+                    <div class="col border">
+                        <h4>081256349078</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
